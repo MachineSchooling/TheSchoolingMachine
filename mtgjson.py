b396a9fd558c364ca76911d4e68399a72e32fdf1
MachineSchooling <wally.wissner@gmail.com>
2016-10-10 14:19:33 -0400

    Update mtgjson.py

--- mtgjson.py
+++ mtgjson.py
@@ -18,7 +18,7 @@ if not os.path.exists("AllCards.json"):
 carddata = json.load(open("AllCards.json", 'r'))
 
 # List of names of every Magic card.
-# Replace retired non-ASCII characters from old cards with their normal English counterparts.
+# Replace retired non-English characters from old cards with their normal English counterparts.
 cardnames = [card.encode('utf-8').replace('\xc3\x86', 'Ae').replace('\xc3\xbb', 'u').replace('\xc3\xba', 'u')
              .replace('\xc3\xa1', 'a').replace('\xc3\xa9', 'e').replace('\xc3\xa0', 'a').replace('\xc3\xa2', 'a')
              .replace('\xc2\xae', '').replace('\xc3\xb6', 'o').replace('\xc3\xad', 'i')
